42cddc8f19bcc6f0a964ea3ec4e89bcf9c4a3bc4
devramon <dsramondev@gmail.com>
2026-06-13 12:34:00 -0300

    feat: estrutura de métodos criada.

---
+++ Services/UsuarioService.cs
@@ -0,0 +1,26 @@
+using UsuarioModel;
+
+class UsuarioService
+{
+    public string arquivo = "Data/usuarios.json";
+
+    public void AdicionarUsuario()
+    {
+        
+    }
+
+    public void AlterarUsuario()
+    {
+        
+    }
+
+    public void ListarUsuarios()
+    {
+        
+    }
+
+    public void ExcluirUsuario()
+    {
+        
+    }
+}
