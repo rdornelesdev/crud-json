936ee20085fc3e500f321e52b505e9111ae8c197
devramon <dsramondev@gmail.com>
2026-06-13 12:34:44 -0300

    feat: seleções do usuário.

---
+++ Services/Main.cs
@@ -0,0 +1,63 @@
+namespace main;
+
+class Main
+{
+    public void MainFnc()
+    {
+        bool validacao = true;
+        while (validacao)
+        {
+            string[] opcoes = ["1. Adicionar usuário", "2. Listar usuários", "3. Alterar usuário específico", "4. Remover usuário específico", "5. Sair"];
+
+            Console.WriteLine("----------");
+            Console.WriteLine("Painel principal");
+            Console.WriteLine("----------");
+
+            foreach (var y in opcoes)
+            {
+                Console.WriteLine(y);
+            }
+
+            Console.WriteLine("Selecione uma opção: ");
+            string opcaoSelecionada = Console.ReadLine()!;
+            int.TryParse(opcaoSelecionada, out int opcaoSelecionadaCast);
+
+            switch (opcaoSelecionadaCast)
+            {
+                case 1:
+                    Console.Clear();
+                    UsuarioService add = new();
+                    add.AdicionarUsuario();
+                    break;
+                
+                case 2:
+                    Console.Clear();
+                    UsuarioService alt = new();
+                    alt.AlterarUsuario();
+                    break;
+                
+                case 3:
+                    Console.Clear();
+                    UsuarioService list = new();
+                    list.ListarUsuarios();
+                    break;
+
+                case 4:
+                    Console.Clear();
+                    UsuarioService del = new();
+                    del.ExcluirUsuario();
+                    break;
+                
+                case 5:
+                    Console.WriteLine("Saindo...");
+                    validacao = false;
+                    break;
+                
+                default:
+                    Console.WriteLine("Opção inválida. Selecione uma opção válida!");
+                    break;
+            }
+
+        }
+    }
+}
